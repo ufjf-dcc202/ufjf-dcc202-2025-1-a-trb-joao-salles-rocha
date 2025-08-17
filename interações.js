259543bf1ce52f0ad2ce814b71af246dd0f5c786
João Salles Rocha <166631994+joaoSallesr@users.noreply.github.com>
2025-08-17 22:19:23 +0000

    Interações básicas com os canteiros (agora é possível: preparar o terreno, regar o terreno preparado, quebrar as pedras e podar as ervas).

--- interações.js
+++ interações.js
@@ -26,7 +26,48 @@ export function escolherFerramenta(id){
 }
 
 export function usarFerramenta(num){
-    console.log(num);
+    const canteiro = document.getElementById("canteiro_"+num);
+    const tipo = document.classList
+    console.log(canteiro);
+
+    switch(ferramenta){
+        case ferramenta = "preparar":
+            if (canteiro.classList.contains('vazio')){
+                canteiro.classList.remove('vazio');
+                canteiro.classList.add('preparado');
+                console.log("Canteiro preparado.");
+            }else
+                console.log("Nada pode ser feito.");
+            break;
+
+       case ferramenta = "regar":
+            if (canteiro.classList.contains('preparado')){
+                canteiro.classList.remove('preparado');
+                canteiro.classList.add('regado');
+                console.log("Canteiro regado.");
+            }else
+                console.log("Nada pode ser feito.");
+            break;
+
+       case ferramenta = "podar":
+            if (canteiro.classList.contains('erva')){
+                canteiro.classList.remove('erva');
+                canteiro.classList.add('vazio');
+                console.log("Erva podada.");
+            }else
+                console.log("Nada pode ser feito.");
+            break;
+
+       case ferramenta = "quebrar":
+            if (canteiro.classList.contains('pedra_1') || canteiro.classList.contains('pedra_2')){
+                canteiro.classList.remove('pedra_1');
+                canteiro.classList.remove('pedra_2');
+                canteiro.classList.add('vazio');
+                console.log("Pedra quebrada.");
+            }else
+                console.log("Nada pode ser feito.");
+            break;
+    }
 }
 
 //Interações extras
